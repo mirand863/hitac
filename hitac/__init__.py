f31b9b1905b0dd8a474d1dac893280864c44657a
Fabio Malcher Miranda <miranda.fmm@gmail.com>
2021-12-03 17:55:00 +0000

    Fix import error if qiime2 is not installed (required for bioconda)

--- hitac/__init__.py
+++ hitac/__init__.py
@@ -6,4 +6,6 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-importlib.import_module("hitac.classifier")
+importlib.import_module("hitac._utils")
+if importlib.util.find_spec("q2_types") is not None:
+    importlib.import_module("hitac.classifier")
