5585a136f5e8241fe8334bcc56275fd0afd638c8
Fabio <miranda.fmm@gmail.com>
2023-01-24 13:25:55 +0100

    Add less_exclusive policy

--- hitac/classifier.py
+++ hitac/classifier.py
@@ -67,7 +67,7 @@ def fit(
     hierarchical_classifier = LocalClassifierPerNode(
         local_classifier=logistic_regression,
         n_jobs=threads,
-        binary_policy="exclusive",
+        binary_policy="less_exclusive",
     )
     hierarchical_classifier.fit(X_train, Y_train)
     return hierarchical_classifier
